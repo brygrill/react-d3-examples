dbef3b0bd2d9754c0c55284bd1be14048f6fa5ba
Bryan Grill <grillb@gmail.com>
2019-06-17 16:13:47 -0400

    init app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container, Segment } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <Container>
+      <Segment>Calendar Heatmap</Segment>
+    </Container>
+  );
+}
+
+export default App;
