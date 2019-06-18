c3c30b23bf0c0525ad6ffc184e3baaa54196fd35
Bryan Grill <grillb@gmail.com>
2019-06-18 14:37:56 -0400

    add react spring

--- src/components/D3.js
+++ src/components/D3.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring';
 import PropTypes from 'prop-types';
 
 const D3 = props => {
+  const animate = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    config: { duration: 1000 },
+  });
+
   return (
-    <svg width={props.width} height={props.height}>
-    <g
-      ref={props.targetRef}
-      transform={`translate(${props.outerRadius} ${props.outerRadius})`}
-    />
-  </svg>
+    <animated.div style={animate}>
+      <svg width={props.width} height={props.height}>
+        <g
+          ref={props.targetRef}
+          transform={`translate(${props.outerRadius} ${props.outerRadius})`}
+        />
+      </svg>
+    </animated.div>
   );
 };
 
