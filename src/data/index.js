2f557a19dd633b08642c07171a9105d0a01f4dc2
Bryan Grill <grillb@gmail.com>
2019-06-18 15:26:21 -0400

    mock calendar data

--- src/data/index.js
+++ src/data/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import moment from 'moment';
 
 const data = {
   pie(r = 5) {
@@ -12,6 +13,17 @@ const data = {
       })
       .value();
   },
+  cal() {
+    return _.chain(365)
+    .range()
+    .map((v, k) => {
+      return {
+        date: moment().subtract(k, 'days').format('YYYY-MM-DD'),
+        value: _.random(10),
+      };
+    })
+    .value();
+  }
 };
 
 export default data;
